1212f3e5b68acdc3b6b612c445fedaffcfc81c0a
katebrighteyes <kate.brighteyes@gmail.com>
2020-01-17 12:50:48 +0900

    first commit

---
+++ darknet/weights/downweight.sh
@@ -0,0 +1,4 @@
+wget https://pjreddie.com/media/files/yolov2.weights
+wget https://pjreddie.com/media/files/yolov3.weights
+wget https://pjreddie.com/media/files/yolov3-tiny.weights
+wget https://pjreddie.com/media/files/yolov2-tiny.weights
